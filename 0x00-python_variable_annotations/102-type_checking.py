69728553cd129f868356caa3284272300e695db3
NjengaC <victorcyrus01@gmail.com>
2024-06-06 23:16:00 +0300

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
-'''TypeChecking'''
 
-from typing import List, Tuple
+"""Module that contains a type-annotated function zoom_array
+that takes a tuple of integers and returns a tuple of integers
+with a factor of 2 or 3.0.
+The type of the input tuple is Tuple and the type of
+the output tuple is Tuple
+"""
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
-    zoomed_in: List[int] = [
+# import Tuple class from the typing module
+from typing import Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """ Change the annotation for 1st and return the type
+        to Tuple
+        to indicate that the function accepts and
+        returns a tuple of any type.
+        Also, change type of array to tuple() instead of list[].
+        Lastly, change the float value to interger
+        when calling zoom_array
+    """
+    zoomed_in: Tuple = tuple(
         item for item in lst
-        for _ in range(factor)
-    ]
+        for i in range(factor)
+    )
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
