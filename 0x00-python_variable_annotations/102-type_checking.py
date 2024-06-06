e83b8c4c13cc84235573b51763c1834d9ca64e51
NjengaC <victorcyrus01@gmail.com>
2024-06-06 23:16:43 +0300

    comments

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -28,8 +28,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
     return zoomed_in
 
 
-array = (12, 72, 91)
+array = (12, 72, 91)  # use tuple instead of list
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)  # use interger value for the factor
