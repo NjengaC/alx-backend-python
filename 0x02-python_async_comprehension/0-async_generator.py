d6d475615e8120efc810327b87b27c5b12522e24
NjengaC <victorcyrus01@gmail.com>
2024-06-11 15:10:05 +0300

    folder location correction

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+''' Module: simple async generator '''
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronous generator coroutine that yields 10 random
+    floating-point numbers between 0 and 10,
+    with a delay of 1 second between each yield.
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
