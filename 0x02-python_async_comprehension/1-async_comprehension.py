8a6a4b1faabeebed937d199b4bd56ce280f58df6
NjengaC <victorcyrus01@gmail.com>
2024-06-11 15:24:31 +0300

    pycode

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,11 @@
-import asyncio
-from typing import List
-from importlib import import_module as using
+#!/usr/bin/env python3
 """
 Coroutine that generates random numbers asynchronously.
 """
+from typing import List
+from importlib import import_module as using
+
+
 async_generator = using('0-async_generator').async_generator
 
 
