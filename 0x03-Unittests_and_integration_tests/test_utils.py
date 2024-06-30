69772f9f944e9dfa4e84d7dbc86e72dc18af15ba
NjengaC <victorcyrus01@gmail.com>
2024-06-30 12:16:12 +0300

    mismatch due to the quotes in the exception message

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module doc
+Unit tests for the utils.access_nested_map function.
 """
 
 import unittest
@@ -9,25 +9,41 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ Test access nested map class """
+    """
+    Test suite for the access_nested_map function.
+    """
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
+    ])
     def test_access_nested_map(self, nested_map, path, expected):
-        """test method"""
+        """
+        Test that access_nested_map returns the expected result
+
+        Args:
+            nested_map (dict): The nested map to access.
+            path (tuple): The path of keys to access in the nested map.
+            expected: The expected value to be returned by the function.
+        """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
-        ])
+    ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """ test exception method """
+        """
+        Test that access_nested_map raises a KeyError for invalid inputs.
+
+        Args:
+            nested_map (dict): The nested map to access.
+            path (tuple): The path of keys to access in the nested map.
+        """
         with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
-        self.assertEqual(str(cm.exception), str(path[-1]))
+        self.assertEqual(str(cm.exception).strip("'"), str(path[-1]))
 
 
 if __name__ == '__main__':
